d1f712d128470639b234366ee9b00e583fd13e01
qiuqidang <1006902479@qq.com>
2018-09-27 14:30:10 +0800

    home页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,41 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import {routerMode} from '../config/env'
+import App from '../App'
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
+const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
+
+const routes = [{
+  path: '/',
+  component: App, // 顶层路由，对应index.html
+  children: [ // 二级路由，对应App.vue
+    // 地址为空时，跳转home页面
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '',
+      redirect: 'home'
+    },
+    // 首页，城市列表页
+    {
+      path: '/home',
+      component: home
     }
   ]
+}]
+
+export default new Router({
+  routes,
+  mode: routerMode,
+  strict: process.env.NODE_ENV !== 'production',
+  scrollBehavior (to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      if (from.meta.keepAlive) {
+        from.meta.savedPosition = document.body.scrollTop
+      }
+      return { x: 0, y: to.meta.savedPosition || 0 }
+    }
+  }
 })
